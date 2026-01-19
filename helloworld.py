7cabd5a1117c52a7b7b5c0d75f7b06afe42fbc32
ryuu <itsinrbiruchannel@gmail.com>
2026-01-19 21:08:34 +0700

    first day

---
+++ helloworld.py
@@ -0,0 +1,29 @@
+# This is the day 1 that i learn how to code in python
+import math
+print(3 + 1) #addition
+print(4 - 2) #substraction
+print(3 * 2) #multiplication
+print(16 % 10) #modulus
+print(5 / 2) #division
+print(4 ** 2) #exponentioal
+print(3 // 2) #floordivition
+
+print('Akito Ryusei') #this will make an output string of my name
+print('Asep') #this will make an output string of my family name
+print('Indonesia') #this will make an output string of my country name
+print('I am enjoying 30 days of python') #this will make an output that i'm enjoying with this
+
+#checking the data types
+print(type(10))
+print(type(9.8))
+print(type(3.14))
+print(type(4-4j))
+print(type(['Asabeneh', 'python', 'Finland']))
+print(type('Akito Ryusei'))
+print(type('Asep'))
+print(type('Indonesia'))
+
+a = (2, 3)
+b = (10, 8)
+
+print(math.dist(a,b))
