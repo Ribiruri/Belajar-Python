2cad7ba8ef4bf52b7f6bf44dd6c49cd1e43cb7fa
ryuu <itsinrbiruchannel@gmail.com>
2026-01-19 22:01:38 +0700

    Good Boy~

---
+++ day_1/helloworld.py
@@ -0,0 +1,30 @@
+#import math lib
+import math
+
+print (1 + 1) #addition
+print (2 - 1) #subtraction
+print (2 * 5) #multiplication
+print (16 % 10) #modulus
+print (5 / 2 ) #division
+print (5 ** 2) #exponential
+print (5 // 2) #floor division
+
+print ('Akito Ryusei') #output name
+print ('Asep') #output name
+print ('Indonesia') #output country
+print ('I am enjoying 30 days of python') #output text
+
+#checking data type
+print (type(10))
+print (type(9.8))
+print (type(3.14))
+print (type(4-4j))
+print (type(['Asabeneh', 'Python', 'Finland']))
+print (type('Akito Ryusei'))
+print (type('Asep'))
+print (type('Indonesia'))
+
+a = (2, 3)
+b = (10, 8)
+
+print (math.dist(a, b))
